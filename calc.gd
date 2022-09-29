aead94e492724eb14c7225e2ca340c6d3e0c7d52
wertomellon <67682200+wertomellon@users.noreply.github.com>
2022-09-28 21:12:25 -0700

    Created Repository/UI
    Set up github repository for the CS480 Project. Created the UI base, no functionality at all yet

---
+++ calc.gd
@@ -0,0 +1,123 @@
+extends Control
+
+
+
+func _ready():
+	pass # Replace with function body.
+
+
+
+func _process(delta):
+	pass
+
+
+func _on_LeftBracket_button_down():
+	pass # Replace with function body.
+
+
+func _on_Log_button_down():
+	pass # Replace with function body.
+
+
+func _on_Sin_button_down():
+	pass # Replace with function body.
+
+
+func _on_Tan_button_down():
+	pass # Replace with function body.
+
+
+func _on_RightBracket_button_down():
+	pass # Replace with function body.
+
+
+func _on_Ln_button_down():
+	pass # Replace with function body.
+
+
+func _on_Cos_button_down():
+	pass # Replace with function body.
+
+
+func _on_Cot_button_down():
+	pass # Replace with function body.
+
+
+func _on_7_button_down():
+	pass # Replace with function body.
+
+
+func _on_4_button_down():
+	pass # Replace with function body.
+
+
+func _on_1_button_down():
+	pass # Replace with function body.
+
+
+func _on_Negative_button_down():
+	pass # Replace with function body.
+
+
+func _on_8_button_down():
+	pass # Replace with function body.
+
+
+func _on_5_button_down():
+	pass # Replace with function body.
+
+
+func _on_2_button_down():
+	pass # Replace with function body.
+
+
+func _on_0_button_down():
+	pass # Replace with function body.
+
+
+func _on_9_button_down():
+	pass # Replace with function body.
+
+
+func _on_6_button_down():
+	pass # Replace with function body.
+
+
+func _on_3_button_down():
+	pass # Replace with function body.
+
+
+func _on_Decimal_button_down():
+	pass # Replace with function body.
+
+
+func _on_LeftParen_button_down():
+	pass # Replace with function body.
+
+
+func _on_Divide_button_down():
+	pass # Replace with function body.
+
+
+func _on_Subtract_button_down():
+	pass # Replace with function body.
+
+
+func _on_Exponent_button_down():
+	pass # Replace with function body.
+
+
+func _on_RightParen_button_down():
+	pass # Replace with function body.
+
+
+func _on_Multiply_button_down():
+	pass # Replace with function body.
+
+
+func _on_Add_button_down():
+	pass # Replace with function body.
+
+
+func _on_Equal_button_down():
+	pass # Replace with function body.
